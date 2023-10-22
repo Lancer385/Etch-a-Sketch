1e4a70e4a291d3d6fd433e51d0dc8cc68c72d7e1
Lancer385 <102915355+Lancer385@users.noreply.github.com>
2023-10-22 20:49:43 +0300

    implement dynamic number of grid rows and columns

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ const container = document.querySelector("#container");
 
 
 function makeGrid(number){
-    for (i = 0; i <= (number + Math.sqrt(number)); i++){
-        let div = document.createElement("div");     
-        div.setAttribute("class", "grid");
+    let counter = 0;    
+    for (i = 0; i <= (number**2 + number); i++){
+        const nth = number + 1;
+        let div = document.createElement("div");
         container.appendChild(div);
+        if (i == counter){
+            counter += nth;
+            continue; // this will skip every (nth + 1) div
+        }
+        div.setAttribute("class", "grid");
     }
 }
 
-makeGrid(256)
+makeGrid(16)
 
 const nodeList = document.querySelectorAll(".grid");
-
+let gridWidth = container.clientWidth / Math.sqrt(nodeList.length);
+let gridHeight = container.clientHeight / Math.sqrt(nodeList.length);
 nodeList.forEach((node) => {
+    node.style.width = `${gridWidth}px`;
+    node.style.height = `${gridHeight}px`;
     node.addEventListener("mouseover", ()=>{
         node.style.backgroundColor = "red";
     })
 })
-
